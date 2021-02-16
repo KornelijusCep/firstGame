cce92653a7c820800c8ead689d839de3a7e793fd
KornelijusCep <51089443+KornelijusCep@users.noreply.github.com>
2021-02-16 21:23:29 +0200

    Added new mechanics to player and new test map
    Player: added triple jump and dash + player movement now independent from camera.
    Map: new testing area for new mechanics

---
+++ Assets/Scripts/DashController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashController : MonoBehaviour
+{
+
+    PlayerController moveScript;
+
+    public float dashSpeed;
+    public float dashTime;
+
+    public float dashCooldown = 5.0f;
+    float timeSinceAction = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveScript = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceAction += Time.deltaTime;
+        if (timeSinceAction > dashCooldown)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                timeSinceAction = 0;
+                StartCoroutine(Dash());
+            }
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+
+        while(Time.time < startTime + dashTime)
+        {
+            float directionY = 0;
+            moveScript.moveDirection.y = directionY;
+            moveScript.controller.Move(moveScript.moveDirection * dashSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
+    public Vector3 moveDirection;
+
     [SerializeField]
     private float speed = 5f;
     [SerializeField]
@@ -18,20 +20,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float tripleJump = 0.5f;
 
+    float changableGravity;
+    float changableSpeed;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    // Let player walk into camera
     private float directionY;
+
     private bool canDoubleJump = false;
     private bool canTripleJump = false;
+
 
     void Start(){
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         controller = GetComponent<CharacterController>();
+        changableSpeed = speed;
     }
-
 
     // Update is called once per frame
     void Update()
@@ -39,12 +46,11 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        Vector3 moveDirection = new Vector3();
+        moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if(moveDirection.magnitude >= 0.1f)
         {     
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
@@ -54,10 +60,11 @@ public class PlayerController : MonoBehaviour
         if (controller.isGrounded)
         {
             canDoubleJump = true;
-            canTripleJump = true;
+            canTripleJump = false;
 
             if (Input.GetButtonDown("Jump"))
             {
+                changableSpeed = speed;
                 directionY = jumpSpeed;
             }
         } 
@@ -66,12 +73,47 @@ public class PlayerController : MonoBehaviour
             {
                 directionY = jumpSpeed * doubleJump;
                 canDoubleJump = false;
+                canTripleJump = true;
             }
+            else if(Input.GetButtonDown("Jump") && canTripleJump)
+            {
+                directionY = jumpSpeed * tripleJump;
+                canTripleJump = false;
+            }
+        } 
+
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            changableGravity = gravity * 10;
+        } 
+        else {
+            changableGravity = speed;
+        } 
+
+        if (Input.GetKey(KeyCode.LeftShift) && controller.isGrounded)
+        {
+            changableSpeed = speed * 1.5f;
+        }
+        else {
+            changableSpeed = speed;
         }
 
-        directionY -= gravity * Time.deltaTime;
+
+        directionY -= changableGravity * Time.deltaTime;
         moveDirection.y = directionY;
-        controller.Move(moveDirection * speed * Time.deltaTime);
+        controller.Move(moveDirection * changableSpeed * Time.deltaTime);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(!controller.isGrounded && hit.normal.y < 0.1f)
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                moveDirection = hit.normal;
+                Debug.DrawRay(hit.point, hit.normal, Color.red, 1.25f);             
+            }
+        }
     }
     
 }
